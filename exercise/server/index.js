b97a97459b2599053bb752271c91d6d393d2b6bd
Jeff <jrbarnid@gmail.com>
2020-07-28 18:50:26 -0400

    Adding error handling to express.

--- exercise/server/index.js
+++ exercise/server/index.js
@@ -3,18 +3,26 @@ const app = express();
 const axios = require("axios");
 
 app.get("/api/defense", async (req, res) => {
-  const result = await axios.get("https://www.defense.gov/data.json");
+  try {
+    const result = await axios.get("https://www.defense.gov/data.json");
 
-  res.send(result.data);
+    res.send(result.data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.get("/api/data", async (req, res) => {
-  // packages are datasets, so this should match the catalog in the readme
-  const result = await axios.get(
-    "http://demo.ckan.org/api/3/action/package_list"
-  );
+  try {
+    // packages are datasets, so this should match the catalog in the readme
+    const result = await axios.get(
+      "http://demo.ckan.org/api/3/action/package_list"
+    );
 
-  res.send(result.data.result);
+    res.send(result.data.result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.listen(8080);
