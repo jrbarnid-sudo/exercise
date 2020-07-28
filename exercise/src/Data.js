11fc18202300629479a02048f02e15121914cb51
Jeff <jrbarnid@gmail.com>
2020-07-28 18:39:22 -0400

    Adding sort to data table.

--- exercise/src/Data.js
+++ exercise/src/Data.js
@@ -3,25 +3,97 @@ import DataTable from "./DataTable";
 import Spinner from "./Spinner";
 
 export default () => {
+  const sorts = { Asc: "asc", Desc: "desc" };
+
+  const sortData = (data, sort) => {
+    switch (sort) {
+      case sorts.Asc:
+        return data.sort();
+      case sorts.Desc:
+        return data.reverse();
+      default:
+        return data.sort();
+    }
+  };
+
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [sort, setSort] = useState(sorts.Asc);
 
   useEffect(() => {
     const fetchData = async () => {
       const results = await (await fetch("/api/data")).json();
 
-      setData(results.result);
+      setData(sortData(results.result, sort));
       setIsLoading(false);
     };
 
     fetchData();
   }, []);
 
+  useEffect(() => {
+    setData(sortData(data, sort));
+    setIsLoading(false);
+  }, [sort]);
+
+  const _setSorts = (e, newSort) => {
+    e.preventDefault();
+
+    if (newSort !== sort) {
+      setIsLoading(true);
+      setSort(newSort);
+    }
+
+    setShowDropdown(false);
+  };
+
+  const content = (
+    <>
+      <div className="py-4 px-4">
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            onClick={() => setShowDropdown(!showDropdown)}
+          >
+            Sort By
+          </button>
+          <div
+            className={`dropdown-menu ${showDropdown ? "show" : ""}`}
+            aria-labelledby="dropdownMenuButton"
+          >
+            <a
+              className="dropdown-item"
+              href="#"
+              onClick={(e) => _setSorts(e, sorts.Asc)}
+            >
+              Title (Asc)
+            </a>
+            <a
+              className="dropdown-item"
+              href="#"
+              onClick={(e) => _setSorts(e, sorts.Desc)}
+            >
+              Title (Desc)
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="col-4">
+        <DataTable packages={data} sort={sort} />
+      </div>
+    </>
+  );
+
   return (
     <div className="py-4">
       <h1 id="data">Data Packages</h1>
-      {isLoading && <Spinner />}
-      <DataTable packages={data} />
+      {isLoading ? <Spinner /> : content}
     </div>
   );
 };
--- exercise/src/DataTable.js
+++ exercise/src/DataTable.js
@@ -1,20 +1,27 @@
 import React from "react";
 
-export default ({ packages }) => {
+export default ({ packages, sort }) => {
   return (
-    <div className="col-4">
-      <table className="table">
-        <tr>
-          {packages.length > 0 && <th>Packages ({packages.length} total)</th>}
-        </tr>
-        {packages.map((item) => {
-          return (
-            <tr>
-              <td>{item}</td>
-            </tr>
-          );
-        })}
-      </table>
-    </div>
+    <table className="table">
+      <tr>
+        {packages.length > 0 && (
+          <th>
+            <div className="d-flex justify-content-between">
+              <div>Packages ({packages.length} total) </div>
+              <div>
+                <span className="text-muted">Packages in {sort} order</span>
+              </div>
+            </div>
+          </th>
+        )}
+      </tr>
+      {packages.map((item) => {
+        return (
+          <tr>
+            <td>{item}</td>
+          </tr>
+        );
+      })}
+    </table>
   );
 };
