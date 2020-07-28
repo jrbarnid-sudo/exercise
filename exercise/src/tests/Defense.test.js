ff1ab9e039b9abf61b2eb0b3038cf5698a805069
Jeff <jrbarnid@gmail.com>
2020-07-28 18:40:54 -0400

    Removing unused tests.

--- exercise/src/tests/Defense.test.js
+++ exercise/src/tests/Defense.test.js
@@ -8,33 +8,9 @@ function setup() {
   return { wrapper, props };
 }
 
-beforeEach(() => {
-  beforeAll(() => {
-    const mockSuccessResponse = {
-      ok: true,
-      json: async () => ({ success: true }),
-    };
-    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
-    const mockFetchPromise = Promise.resolve({
-      json: () => mockJsonPromise,
-    });
-    jest.spyOn(global, "fetch").mockImplementation(() => mockFetchPromise);
-  });
-});
-
 describe("Defense Route Tests", () => {
   it("Should render", () => {
     const { wrapper } = setup();
     expect(wrapper.find("#defense").exists()).toBe(true);
-  });
-
-  it("Should fetch data", (done) => {
-    setTimeout(() => {
-      const { wrapper } = setup();
-
-      expect(global.fetch).toHaveBeenCalledTimes(1);
-
-      done();
-    }, 100);
   });
 });
