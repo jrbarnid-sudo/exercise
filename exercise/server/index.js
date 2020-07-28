4d4b2c4306ab0270193e97c3fca50295cd7ea693
Jeff <jrbarnid@gmail.com>
2020-07-28 16:13:06 -0400

    Adding API calls for data and defense.
    Implemented API calls in express for calling the
    open source APIs in the readme.

--- exercise/server/index.js
+++ exercise/server/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
-app.get("/hey", (req, res) => res.send("ho!"));
+app.get("/api/defense", async (req, res) => {
+  const result = await axios.get("https://www.defense.gov/data.json");
+
+  res.send(result.data);
+});
+
+app.get("/api/data", async (req, res) => {
+  // packages are datasets, so this should match the catalog in the readme
+  const result = await axios.get(
+    "http://demo.ckan.org/api/3/action/package_list"
+  );
+
+  res.send(result.data.result);
+});
 
 app.listen(8080);
