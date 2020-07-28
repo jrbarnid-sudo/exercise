51dc3a58d108b42113c26b43ec0c3a1da42e094e
Jeff <jrbarnid@gmail.com>
2020-07-28 17:06:09 -0400

    Add base navbar to app home.

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container-fluid">
+      <nav className="d-flex">
+        <a class="navbar-brand" href="/">
+          Exercise
         </a>
-      </header>
+
+        <div class="navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto list-group-horizontal">
+            <li class="nav-item px-4">
+              <a class="nav-link" href="#">
+                Defense
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Data
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
