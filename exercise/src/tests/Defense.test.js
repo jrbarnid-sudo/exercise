4a650e5cd887b321f8660d9d966b1bddb7989a51
Jeff <jrbarnid@gmail.com>
2020-07-28 16:40:10 -0400

    Adding failing test for fetching defense data.

--- exercise/src/tests/Defense.test.js
+++ exercise/src/tests/Defense.test.js
@@ -4,13 +4,37 @@ import Defense from "../Defense";
 
 function setup() {
   const props = {};
-  const wrapper = shallow(<Defense />);
+  const wrapper = mount(<Defense />);
   return { wrapper, props };
 }
+
+beforeEach(() => {
+  beforeAll(() => {
+    const mockSuccessResponse = {
+      ok: true,
+      json: async () => ({ success: true }),
+    };
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise,
+    });
+    jest.spyOn(global, "fetch").mockImplementation(() => mockFetchPromise);
+  });
+});
 
 describe("Defense Route Tests", () => {
   it("Should render", () => {
     const { wrapper } = setup();
     expect(wrapper.find("#defense").exists()).toBe(true);
+  });
+
+  it("Should fetch data", (done) => {
+    setTimeout(() => {
+      const { wrapper } = setup();
+
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+
+      done();
+    }, 100);
   });
 });
