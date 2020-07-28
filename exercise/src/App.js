62686bee2918ee0e3aa441bc299ec9e34aeb57ad
Jeff <jrbarnid@gmail.com>
2020-07-28 17:13:57 -0400

    Adding basic routing.

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -1,33 +1,37 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Defense from "./Defense";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container-fluid">
-      <nav className="d-flex">
-        <a class="navbar-brand" href="/">
-          Exercise
-        </a>
+    <Router>
+      <div className="container-fluid">
+        <nav className="d-flex">
+          <a class="navbar-brand" href="/">
+            Exercise
+          </a>
 
-        <div class="navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto list-group-horizontal">
-            <li class="nav-item px-4">
-              <a class="nav-link" href="#">
-                Defense
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Data
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+          <div class="d-flex navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto list-group-horizontal">
+              <li class="nav-item px-4">
+                <Link to="/defense">Defense</Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/data">Data</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Switch>
+          <Route path="/defense">
+            <Defense />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
